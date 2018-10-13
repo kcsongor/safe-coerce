ced166ac28cfbbad70f9a0266897e22aeddba5bb
chessai <chessai1996@gmail.com>
2018-10-13 12:43:12 -0400

    add safe coerce#

--- src/Safe/Coerce.hs
+++ src/Safe/Coerce.hs
@@ -1,6 +1,12 @@
-module Safe.Coerce (coerce) where
+{-# LANGUAGE MagicHash #-}
+
+module Safe.Coerce (coerce, coerce#) where
 
 import Unsafe.Coerce (unsafeCoerce)
+import GHC.Exts (unsafeCoerce#, TYPE, RuntimeRep(UnliftedRep))
 
 coerce :: a -> b
 coerce = unsafeCoerce
+
+coerce# :: TYPE 'UnliftedRep -> TYPE 'UnliftedRep
+coerce# = unsafeCoerce#
